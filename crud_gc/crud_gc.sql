75cd00a29e97186452aa9cf5152f3b7f89516c45
YogiWiguna <58981369+YogiWiguna@users.noreply.github.com>
2021-01-16 13:43:12 +0800

    a
    a

---
+++ crud_gc/crud_gc.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jan 2021 pada 14.48
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud_gc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(11) NOT NULL,
+  `nama_produk` varchar(255) DEFAULT NULL,
+  `stock` int(10) NOT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `harga_beli` int(11) DEFAULT NULL,
+  `harga_jual` int(11) DEFAULT NULL,
+  `gambar_produk` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`id`, `nama_produk`, `stock`, `deskripsi`, `harga_beli`, `harga_jual`, `gambar_produk`) VALUES
+(1, 'Indomie', 15, 'indomie ini enak sekali sob', 4000, 5000, '489-contoh.jpg'),
+(8, '4', 5, 'Mie sedaap enak', 4500, 5500, '43-sedaap.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ crud_gc/tambah_produk.php
@@ -0,0 +1,94 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Indah flower shop</title>
+    <style type="text/css">
+       body {
+      background-color:pink;
+      background-image:url(URLGAMBAR);
+      }
+      * {
+        font-family: "Trebuchet MS";
+      }
+      h1 {
+        text-transform: uppercase;
+        color: purple;
+      }
+    button {
+          background-color: purple;
+          color: #fff;
+          padding: 10px;
+          text-decoration: none;
+          font-size: 12px;
+          border: 0px;
+          margin-top: 20px;
+    }
+    label {
+      margin-top: 10px;
+      float: left;
+      text-align: left;
+      width: 100%;
+    }
+    input {
+      padding: 6px;
+      width: 100%;
+      box-sizing: border-box;
+      background:#B0E0E6;
+      border: 2px solid #fff;
+      outline-color: purple;
+    }
+    div {
+      width: 100%;
+      height: auto;
+    }
+    .base {
+      width: 400px;
+      height: auto;
+      padding: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      background: #ededed;
+    }
+    </style>
+  </head>
+  <body>
+      <center>
+        <h1>Tambah Produk</h1>
+      <center>
+      <form method="POST" action="proses_tambah.php" enctype="multipart/form-data" >
+      <section class="base">
+        <div>
+          <label>Nama Produk</label>
+          <input type="text" name="nama_produk" autofocus="" required="" />
+        </div>
+        <div>
+          <label>Stock</label>
+          <input type="text" name="stock" autofocus="" required="" />
+        </div>
+        <div>
+          <label>Deskripsi</label>
+         <input type="text" name="deskripsi" />
+        </div>
+        <div>
+          <label>Harga Beli</label>
+         <input type="text" name="harga_beli" required="" />
+        </div>
+        <div>
+          <label>Harga Jual</label>
+         <input type="text" name="harga_jual" required="" />
+        </div>
+        <div>
+          <label>Gambar Produk</label>
+         <input type="file" name="gambar_produk" required="" />
+        </div>
+        <div>
+         <button type="submit">Simpan Produk</button>
+        </div>
+        </section>
+      </form>
+  </body>
+</html>
